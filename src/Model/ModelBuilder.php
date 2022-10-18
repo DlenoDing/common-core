c658f97dec8d3c0f650e0dda562eab498450fba7
Dleno <dleno@126.com>
2022-10-18 21:16:35 +0800

    init

--- src/Model/ModelBuilder.php
+++ src/Model/ModelBuilder.php
@@ -65,6 +65,25 @@ class ModelBuilder extends Builder
     }
 
     /**
+     * @param string $name
+     * @return $this
+     */
+    public function setConnection($name)
+    {
+        $this->model->setConnection($name);
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getConnectionName()
+    {
+        return $this->model->getConnectionName();
+    }
+
+    /**
      * Find a model by its primary key or return fresh model instance.
      *
      * @param array $columns
